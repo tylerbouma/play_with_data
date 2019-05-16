ac0c17352e6291ef851b40d7e8356a82c317f953
Yallow <tylerbouma1@gmail.com>
2019-05-15 22:05:30 -0500

    get mean of five different age groups

--- map.py
+++ map.py
@@ -4,10 +4,14 @@ import statistics
 
 f = pd.read_csv('heart.csv', delimiter=',')
 
-old = f[f.age>=60]
-young = f[f.age<60]
+sixty = f[f.age>=60]
+fifty = f[(f.age>=50) & (f.age<60)]
+forty = f[(f.age>=40) & (f.age<50)]
+thirty = f[(f.age>=30) & (f.age<40)]
+young = f[f.age<30]
 
-# find the mean chol of everyone older than 60
-print(statistics.mean(old.chol))
-# find the mean chol of everyone younger than 60
-print(statistics.mean(young.chol))
+print(int(statistics.mean(sixty.chol)))
+print(int(statistics.mean(fifty.chol)))
+print(int(statistics.mean(forty.chol)))
+print(int(statistics.mean(thirty.chol)))
+print(int(statistics.mean(young.chol)))
